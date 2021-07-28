9973ca7a5b4f653408b584bb9d8718993158c133
유도혁 <okdohyuk@gmail.com>
2021-07-28 13:41:51 +0900

    feat: react router dom

---
+++ blog-web/src/components/Card.tsx
@@ -0,0 +1,11 @@
+import React, { FC } from "react";
+
+interface CardProps {
+    title: string;
+}
+
+const Card: FC<CardProps> = ({ title }) => {
+    return <div>{title}</div>;
+};
+
+export default Card;
