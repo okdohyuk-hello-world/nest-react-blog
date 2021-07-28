d98baa6989dda1398f81d8a3c5fff6322e2c23be
유도혁 <okdohyuk@gmail.com>
2021-07-28 14:55:59 +0900

    feat: styles

--- blog-web/src/components/Card.tsx
+++ blog-web/src/components/Card.tsx
@@ -1,11 +1,18 @@
-import React, { FC } from "react";
+import React from "react";
+import { FC } from "react";
 
 interface CardProps {
     title: string;
+    content: string;
+    thumbnail: string;
 }
 
-const Card: FC<CardProps> = ({ title }) => {
-    return <div>{title}</div>;
+export const Card: FC<CardProps> = ({ title, content, thumbnail }) => {
+    return (
+        <button className="text-left">
+            <img src={thumbnail} alt="thumbnail" />
+            <p className="text-gray-800 text-xl">{title}</p>
+            <p className="text-gray-500 text-sm truncate">{content}</p>
+        </button>
+    );
 };
-
-export default Card;
--- blog-web/src/pages/HomePage.tsx
+++ blog-web/src/pages/HomePage.tsx
@@ -1,17 +1,25 @@
 import React from "react";
-import Card from "../components/Card";
+import { Card } from "../components/Card";
 import { articles } from "../dummy";
 
 export const HomePage = () => {
     return (
-        <>
-            <h1>블로그</h1>
-            <p>블로그 입니다.</p>
-            <div>
+        <div className="max-w-screen-lg mx-auto">
+            <div className="flex items-end">
+                <h1 className="text-4xl text-gray-800">블로그</h1>
+                <p className="ml-4 text-xl text-gray-500">제 블로그입니다.</p>
+            </div>
+
+            <div className="grid grid-cols-3 gap-x-2 gap-y-4 m-2">
                 {articles.map((article) => (
-                    <Card key={article.id} title={article.title} />
+                    <Card
+                        key={article.id}
+                        title={article.title}
+                        content={article.content}
+                        thumbnail={article.thumbnail}
+                    />
                 ))}
             </div>
-        </>
+        </div>
     );
 };
